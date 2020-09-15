6e1b1b484bf0357ef28fc931e9239b7e4b548a64
talkpik <talkpik2020@gmail.com>
2020-09-14 19:39:33 -0700

    Updates

---
+++ precache-manifest.226da8c70d773b1031bf682323fada69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6213d018496ad842f2fd9a9d12bfe561",
+    "url": "/tp4_public/index.html"
+  },
+  {
+    "revision": "ebd5e49ee590661babe1",
+    "url": "/tp4_public/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "837065af155e868195ad",
+    "url": "/tp4_public/static/js/2.e02a7a3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tp4_public/static/js/2.e02a7a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd5e49ee590661babe1",
+    "url": "/tp4_public/static/js/main.a9ab65d0.chunk.js"
+  },
+  {
+    "revision": "4aff5bbb0cd89dfaefcb",
+    "url": "/tp4_public/static/js/runtime-main.ab4253ce.js"
+  }
+]);
