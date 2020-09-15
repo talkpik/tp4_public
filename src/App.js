e9ef6c13e43881e35e12f07e58dd8d98d3bb3bce
talkpik <talkpik2020@gmail.com>
2020-09-14 19:37:51 -0700

    First test commit to tp4

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Header from './Header';
+import Services from './Services';
+import Seeker from './Seeker';
+import Speaker from './Speaker';
+import Events from './Events';
+import Team from './Team';
+import Contact from './Contact';
+import Footer from './Footer';
+
+class App extends Component {
+
+  
+	render(){
+	
+	return (
+		
+	  <div className="App">
+  	  <Navbar/>
+	  <Header/>
+	  <Services/>
+	  <Seeker/>
+	  <Speaker/>
+	  <Events/>
+	  <Team/>
+	  <Contact/>
+	  <Footer/>
+  </div>
+		  );
+}
+}
+export default App;
+
+
+
+  
